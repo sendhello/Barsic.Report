65b305453f9d65d95ccfb227ca807edda4d53dc8
sendhello <sendhello@yahoo.com>
2018-08-13 11:57:39 +0400

    Initial commit

---
+++ barsicreport2.py
@@ -0,0 +1,338 @@
+# -*- coding: utf-8 -*-
+#
+# This file created with KivyCreatorProject
+# <https://github.com/HeaTTheatR/KivyCreatorProgect
+#
+# Copyright © 2017 Easy
+#
+# For suggestions and questions:
+# <kivydevelopment@gmail.com>
+# 
+# LICENSE: MIT
+
+import os
+import sys
+from ast import literal_eval
+import logging
+from datetime import datetime, timedelta
+
+from kivy.app import App
+from kivy.uix.modalview import ModalView
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.config import ConfigParser
+from kivy.clock import Clock
+from kivy.utils import get_color_from_hex, get_hex_from_color
+from kivy.metrics import dp
+from kivy.properties import ObjectProperty, StringProperty
+
+from main import __version__
+from libs.translation import Translation
+from libs.uix.baseclass.startscreen import StartScreen
+from libs.uix.lists import Lists
+from libs.utils.showplugins import ShowPlugins
+
+from kivymd.theming import ThemeManager
+from kivymd.label import MDLabel
+from kivymd.time_picker import MDTimePicker
+from kivymd.date_picker import MDDatePicker
+
+from toast import toast
+from dialogs import card
+
+
+class BarsicReport2(App):
+    """
+    Функционал программы.
+    """
+
+    title = 'Барсик.Отчеты'
+    icon = 'icon.png'
+    nav_drawer = ObjectProperty()
+    theme_cls = ThemeManager()
+    theme_cls.primary_palette = 'Purple'
+    theme_cls.theme_style = 'Light'
+    lang = StringProperty('ru')
+
+    previous_date = ObjectProperty()
+
+    def __init__(self, **kvargs):
+        super(BarsicReport2, self).__init__(**kvargs)
+        Window.bind(on_keyboard=self.events_program)
+        Window.soft_input_mode = 'below_target'
+
+        self.list_previous_screens = ['base']
+        self.window = Window
+        self.plugin = ShowPlugins(self)
+        self.config = ConfigParser()
+        self.manager = None
+        self.window_language = None
+        self.exit_interval = False
+        self.dict_language = literal_eval(
+            open(
+                os.path.join(self.directory, 'data', 'locales', 'locales.txt')).read()
+        )
+        self.translation = Translation(
+            self.lang, 'Ttest', os.path.join(self.directory, 'data', 'locales')
+        )
+
+    def get_application_config(self):
+        return super(BarsicReport2, self).get_application_config(
+                        '{}/%(appname)s.ini'.format(self.directory))
+
+    def build_config(self, config):
+        '''Создаёт файл настроек приложения barsicreport2.ini.'''
+
+        config.adddefaultsection('General')
+        config.setdefault('General', 'language', 'en')
+
+    def set_value_from_config(self):
+        '''Устанавливает значения переменных из файла настроек barsicreport2.ini.'''
+
+        self.config.read(os.path.join(self.directory, 'barsicreport2.ini'))
+        self.lang = self.config.get('General', 'language')
+
+    def build(self):
+        self.set_value_from_config()
+        self.load_all_kv_files(os.path.join(self.directory, 'libs', 'uix', 'kv'))
+        self.screen = StartScreen()  # главный экран программы
+        self.manager = self.screen.ids.manager
+        self.nav_drawer = self.screen.ids.nav_drawer
+
+        return self.screen
+
+    def load_all_kv_files(self, directory_kv_files):
+        for kv_file in os.listdir(directory_kv_files):
+            kv_file = os.path.join(directory_kv_files, kv_file)
+            if os.path.isfile(kv_file):
+                with open(kv_file, encoding='utf-8') as kv:
+                    Builder.load_string(kv.read())
+
+    def events_program(self, instance, keyboard, keycode, text, modifiers):
+        '''Вызывается при нажатии кнопки Меню или Back Key
+        на мобильном устройстве.'''
+
+        if keyboard in (1001, 27):
+            if self.nav_drawer.state == 'open':
+                self.nav_drawer.toggle_nav_drawer()
+            self.back_screen(event=keyboard)
+        elif keyboard in (282, 319):
+            pass
+
+        return True
+
+    def back_screen(self, event=None):
+        '''Менеджер экранов. Вызывается при нажатии Back Key
+        и шеврона "Назад" в ToolBar.'''
+
+        # Нажата BackKey.
+        if event in (1001, 27):
+            if self.manager.current == 'base':
+                self.dialog_exit()
+                return
+            try:
+                self.manager.current = self.list_previous_screens.pop()
+            except:
+                self.manager.current = 'base'
+            self.screen.ids.action_bar.title = self.title
+            self.screen.ids.action_bar.left_action_items = \
+                [['menu', lambda x: self.nav_drawer._toggle()]]
+
+    def show_plugins(self, *args):
+        '''Выводит на экран список плагинов.'''
+
+        self.plugin.show_plugins()
+
+    def show_about(self, *args):
+        self.nav_drawer.toggle_nav_drawer()
+        self.screen.ids.about.ids.label.text = \
+            self.translation._(
+                u'[size=20][b]BarsicReport2[/b][/size]\n\n'
+                u'[b]Version:[/b] {version}\n'
+                u'[b]License:[/b] MIT\n\n'
+                u'[size=20][b]Developer[/b][/size]\n\n'
+                u'[ref=SITE_PROJECT]'
+                u'[color={link_color}]NAME_AUTHOR[/color][/ref]\n\n'
+                u'[b]Source code:[/b] '
+                u'[ref=REPO_PROJECT]'
+                u'[color={link_color}]GitHub[/color][/ref]').format(
+                version=__version__,
+                link_color=get_hex_from_color(self.theme_cls.primary_color)
+            )
+        self.manager.current = 'about'
+        self.screen.ids.action_bar.left_action_items = \
+            [['chevron-left', lambda x: self.back_screen(27)]]
+
+    def show_reports(self, *args):
+        self.nav_drawer.toggle_nav_drawer()
+        self.manager.current = 'report'
+        self.screen.ids.action_bar.left_action_items = \
+            [['chevron-left', lambda x: self.back_screen(27)]]
+
+    def show_license(self, *args):
+        self.screen.ids.license.ids.text_license.text = \
+            self.translation._('%s') % open(
+                os.path.join(self.directory, 'LICENSE'), encoding='utf-8').read()
+        self.nav_drawer._toggle()
+        self.manager.current = 'license'
+        self.screen.ids.action_bar.left_action_items = \
+            [['chevron-left', lambda x: self.back_screen(27)]]
+        self.screen.ids.action_bar.title = \
+            self.translation._('MIT LICENSE')
+
+    def select_locale(self, *args):
+        '''Выводит окно со списком имеющихся языковых локализаций для
+        установки языка приложения.'''
+
+        def select_locale(name_locale):
+            '''Устанавливает выбранную локализацию.'''
+
+            for locale in self.dict_language.keys():
+                if name_locale == self.dict_language[locale]:
+                    self.lang = locale
+                    self.config.set('General', 'language', self.lang)
+                    self.config.write()
+
+        dict_info_locales = {}
+        for locale in self.dict_language.keys():
+            dict_info_locales[self.dict_language[locale]] = \
+                ['locale', locale == self.lang]
+
+        if not self.window_language:
+            self.window_language = card(
+                Lists(
+                    dict_items=dict_info_locales,
+                    events_callback=select_locale, flag='one_select_check'
+                ),
+                size=(.85, .55)
+            )
+        self.window_language.open()
+
+    def dialog_exit(self):
+        def check_interval_press(interval):
+            self.exit_interval += interval
+            if self.exit_interval > 5:
+                self.exit_interval = False
+                Clock.unschedule(check_interval_press)
+
+        if self.exit_interval:
+            sys.exit(0)
+            
+        Clock.schedule_interval(check_interval_press, 1)
+        toast(self.translation._('Press Back to Exit'))
+
+    def on_lang(self, instance, lang):
+        self.translation.switch_lang(lang)
+
+    def get_time_picker_data(self, instance, time):
+        self.root.ids.time_picker_label.text = str(time)
+        self.previous_time = time
+
+    def show_time_picker(self):
+        self.time_dialog = MDTimePicker()
+        self.time_dialog.bind(time=self.get_time_picker_data)
+        if self.root.ids.time_picker_use_previous_time.active:
+            try:
+                self.time_dialog.set_time(self.previous_time)
+            except AttributeError:
+                pass
+        self.time_dialog.open()
+
+    def set_date_from(self, date_obj):
+        self.previous_date = date_obj
+        self.root.ids.report.ids.date_from.text = str(date_obj)
+
+    def show_date_from(self):
+        pd = self.previous_date
+        try:
+            MDDatePicker(self.set_date_from,
+                         pd.year, pd.month, pd.day).open()
+        except AttributeError:
+            MDDatePicker(self.set_date_from).open()
+
+    def set_date_to(self, date_obj):
+        self.previous_date = date_obj
+        self.root.ids.report.ids.date_to.text = str(date_obj)
+
+    def show_date_to(self):
+        pd = self.previous_date
+        try:
+            MDDatePicker(self.set_date_to,
+                         pd.year, pd.month, pd.day).open()
+        except AttributeError:
+            MDDatePicker(self.set_date_to).open()
+
+    def click_date_switch(self):
+        if self.root.ids.report.ids.date_switch.active:
+            self.root.ids.report.ids.label_date.text = 'Дата:'
+            self.root.ids.report.ids.date_to.text = self.show_next_day()
+        else:
+            self.root.ids.report.ids.label_date.text = 'Период:'
+
+    def show_today(self):
+        return datetime.now().strftime("%Y-%m-%d")
+
+    def show_next_day(self):
+        try:
+            # day = datetime.strptime('%Y-%m-%d', self.root.ids.report.ids.date_from.text)
+            day = datetime.strptime(self.root.ids.report.ids.date_from.text, "%Y-%m-%d")
+        except AttributeError:
+            day = datetime.now()
+        return (day + timedelta(1)).strftime("%Y-%m-%d")
+
+
+    def count_clients(self):
+        """
+        Количество человек в зоне
+        :return: Количество человек в зоне
+        """
+        import pyodbc
+
+        logging.info(f'{str(datetime.now()):25}:    Выполнение функции "count_clients" с параметрами (!)[ПАРАМЕТРЫ]')
+
+        cnxn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER=192.168.1.1\SKISRV;DATABASE=AquaPark_Ulyanovsk;UID=sa;PWD=datakrat')
+        cursor = cnxn.cursor()
+
+        cursor.execute("""
+                        SELECT
+                            [gr].[c1] as [c11],
+                            [gr].[StockCategory_Id] as [StockCategory_Id1],
+                            [c].[Name],
+                            [c].[NN]
+                        FROM
+                            (
+                                SELECT
+                                    [_].[CategoryId] as [StockCategory_Id],
+                                    Count(*) as [c1]
+                                FROM
+                                    [AccountStock] [_]
+                                        INNER JOIN [SuperAccount] [t1] ON [_].[SuperAccountId] = [t1].[SuperAccountId]
+                                WHERE
+                                    [_].[StockType] = 41 AND
+                                    [t1].[Type] = 0 AND
+                                    [_].[Amount] > 0 AND
+                                    NOT ([t1].[IsStuff] = 1)
+                                GROUP BY
+                                    [_].[CategoryId]
+                            ) [gr]
+                                INNER JOIN [Category] [c] ON [gr].[StockCategory_Id] = [c].[CategoryId]
+                       """)
+        result = []
+        while True:
+            row = cursor.fetchone()
+            if row:
+                result.append(row)
+            else:
+                break
+        logging.info(f'{str(datetime.now()):25}:    Результат функции "count_clients": {result}')
+        if not result:
+            result.append(('Пусто', 488, '', '0003'))
+        return result
+
+    def count_clients_reload(self, count_clients):
+        self.screen.ids.base.ids.name_zone.text = str(count_clients[0][2])
+        self.screen.ids.base.ids.count.text = str(count_clients[0][0])
+
+# if __name__ == '__main__':
+#     print(DEVICE_TYPE)

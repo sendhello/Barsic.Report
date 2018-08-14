498296b91d25def59973c8b43d7c4f76b9675ee6
sendhello <sendhello@yahoo.com>
2018-08-14 20:20:08 +0400

    Добавлен функционал Запросов в базы Барс и битрикс и формирование словарей отчетов

--- barsicreport2.py
+++ barsicreport2.py
@@ -16,7 +16,7 @@ from ast import literal_eval
 import logging
 from datetime import datetime, timedelta
 import pyodbc
-import re
+from decimal import Decimal
 
 from kivy.app import App
 from kivy.uix.modalview import ModalView
@@ -80,7 +80,7 @@ class BarsicReport2(App):
         self.translation = Translation(
             self.lang, 'Ttest', os.path.join(self.directory, 'data', 'locales')
         )
-        self.date_from = datetime.now()
+        self.date_from = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
         self.date_to = self.date_from + timedelta(1)
 
         self.org1 = ''
@@ -104,6 +104,7 @@ class BarsicReport2(App):
         config.setdefault('MSSQL', 'pwd', 'password')
         config.setdefault('MSSQL', 'database1', 'database')
         config.setdefault('MSSQL', 'database2', 'database')
+        config.setdefault('MSSQL', 'database_bitrix', 'database')
 
     def set_value_from_config(self):
         '''Устанавливает значения переменных из файла настроек barsicreport2.ini.'''
@@ -116,6 +117,7 @@ class BarsicReport2(App):
         self.pwd = self.config.get('MSSQL', 'pwd')
         self.database1 = self.config.get('MSSQL', 'database1')
         self.database2 = self.config.get('MSSQL', 'database2')
+        self.database_bitrix = self.config.get('MSSQL', 'database_bitrix')
 
     def build(self):
         self.set_value_from_config()
@@ -331,7 +333,7 @@ class BarsicReport2(App):
         try:
             self.date_to = datetime.strptime(self.root.ids.report.ids.date_from.text, "%Y-%m-%d") + timedelta(1)
         except AttributeError:
-            self.date_to = datetime.now() + timedelta(1)
+            self.date_to = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d') + timedelta(1)
         logging.info(f'{str(datetime.now()):25}:    Установка периода отчета на {self.date_from} - {self.date_to}')
         return self.date_to.strftime("%Y-%m-%d")
 
@@ -339,7 +341,7 @@ class BarsicReport2(App):
         try:
             self.date_from = datetime.strptime(self.root.ids.report.ids.date_to.text, "%Y-%m-%d") - timedelta(1)
         except AttributeError:
-            self.date_from = datetime.now()
+            self.date_from = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
         return self.date_from.strftime("%Y-%m-%d")
 
     def count_clients(
@@ -506,6 +508,203 @@ class BarsicReport2(App):
             self.show_dialog(f'Невозможно открыть {database}', repr(e))
         return result
 
+    def itog_report(
+            self,
+            server,
+            database,
+            driver,
+            user,
+            pwd,
+            org,
+            date_from,
+            date_to,
+            hide_zeroes='0',
+            hide_internal='1',
+    ):
+        """
+        Делает запрос в базу Барс и возвращает итоговый отчет за запрашиваемый период
+        :param server: str - Путь до MS-SQL сервера, например 'SQLEXPRESS\BASE'
+        :param database: str - Имя базы данных Барса, например 'SkiBars2'
+        :param uid: str - Имя пользователя базы данных, например 'sa'
+        :param pwd: str - Пароль пользователя базы данных, например 'pass'
+        :param sa: str - Id организации в Барсе
+        :param date_from: str - Начало отчетного периода в формате: 'YYYYMMDD 00:00:00'
+        :param date_to:  str - Конец отчетного периода в формате: 'YYYYMMDD 00:00:00'
+        :param hide_zeroes: 0 or 1 - Скрывать нулевые позиции?
+        :param hide_internal: 0 or 1 - Скрывать внутренние точки обслуживания?
+        :return: Итоговый отчет
+        """
+        cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={user};PWD={pwd}')
+        date_from = date_from.strftime('%Y%m%d 00:00:00')
+        date_to = date_to.strftime('%Y%m%d 00:00:00')
+        cursor = cnxn.cursor()
+        cursor.execute(
+            f"exec sp_reportOrganizationTotals_v2 @sa={org},@from='{date_from}',@to='{date_to}',@hideZeroes={hide_zeroes},"
+            f"@hideInternal={hide_internal}")
+        report = []
+        while True:
+            row = cursor.fetchone()
+            if row:
+                report.append(row)
+            else:
+                break
+        if len(report) > 1:
+            logging.info(f'{str(datetime.now())[:-7]}: Отчет сформирован ID организации = {org}, '
+                         f'Период: {date_from[:8]}-{date_to[:8]}, Скрывать нули = {hide_zeroes}, .'
+                         f'Скрывать внутренние точки обслуживания: {hide_internal})')
+        return report
+
+    def itog_report_convert_to_dict(self, report):
+        """
+        Преобразует список кортежей отчета в словарь
+        :param report: list - Итоговый отчет в формате списка картежей полученный из функции full_report
+        :return: dict - Словарь услуг и их значений
+        """
+        result = {}
+        for row in report:
+            result[row[4]] = (row[1], row[0])
+        return result
+
+    def add_sum(self, report):
+        """
+        Расчитывает и добавляет к словарю-отчету 1 элемент: Итого
+        :param report: dict - словарь-отчет
+        :return: dict - словарь-отчет
+        """
+        sum_service = Decimal(0)
+        sum_many = Decimal(0)
+        for line in report:
+            if not (report[line][0] is None or report[line][0] is None):
+                if line != 'Депозит':
+                    sum_service += report[line][0]
+                sum_many += report[line][1]
+        report['Итого по отчету'] = (sum_service, sum_many)
+        return report
+
+    def decimal_to_googlesheet(self, dict):
+        """
+        Преобразует суммы Decimal в float
+        :param dict:
+        :return:
+        """
+        new_dict = {}
+        for key in dict:
+            if type(dict[key][0]) is Decimal:
+                new_dict[key] = (int(dict[key][0]), float(dict[key][1]))
+            else:
+                new_dict[key] = (dict[key][0], dict[key][1])
+        return new_dict
+
+    def read_bitrix_base(self,
+                         server,
+                         database,
+                         user,
+                         pwd,
+                         driver,
+                         date_from,
+                         date_to,
+                         ):
+        """
+        Функция делает запрос в базу и возвращает список продаж за аказанную дату
+        :param server: str - Путь до MS-SQL сервера, например 'SQLEXPRESS\BASE'
+        :param database: str - Имя базы данных Барса, например 'SkiBars2'
+        :param uid: str - Имя пользователя базы данных, например 'sa'
+        :param pwd: str - Пароль пользователя базы данных, например 'pass'
+        :param date_from: str - Начало отчетного периода в формате: 'YYYYMMDD 00:00:00'
+        :param date_to:  str - Конец отчетного периода в формате: 'YYYYMMDD 00:00:00'
+        :return: list = Список организаций, каджая из которых - кортеж с параметрами организации
+        """
+        date_from = (date_from - timedelta(1)).strftime("%Y%m%d") + " 19:00:00"
+        date_to = (date_to - timedelta(1)).strftime("%Y%m%d") + " 19:00:00"
+
+        cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={user};PWD={pwd}')
+        cursor = cnxn.cursor()
+
+        cursor.execute(
+            f"""
+            SELECT 
+                Id, OrderNumber, ProductId, ProductName, OrderDate, PayDate, Sum, Pay, Status, Client
+            FROM 
+                Transactions
+            WHERE
+                (OrderDate >= '{date_from}')and(OrderDate < '{date_to}')
+            """)
+        orders = []
+        while True:
+            row = cursor.fetchone()
+            if row:
+                orders.append(row)
+            else:
+                break
+
+        summ = 0
+        for order in orders:
+            summ += float(order[6])
+        return len(orders), summ
+
+    def run_report(self):
+        """
+        Выполнить отчеты
+        :return:
+        """
+        itog_report_org1 = None
+        itog_report_org2 = None
+        report_bitrix = None
+
+        if self.org1:
+            itog_report_org1 = self.itog_report(
+                server=self.server,
+                database=self.database1,
+                driver=self.driver,
+                user=self.user,
+                pwd=self.pwd,
+                org=self.org1[0],
+                date_from=self.date_from,
+                date_to=self.date_to,
+                hide_zeroes='0',
+                hide_internal='1',
+            )
+        if self.org2:
+            itog_report_org2 = self.itog_report(
+                server=self.server,
+                database=self.database2,
+                driver=self.driver,
+                user=self.user,
+                pwd=self.pwd,
+                org=self.org2[0],
+                date_from=self.date_from,
+                date_to=self.date_to,
+                hide_zeroes='0',
+                hide_internal='1',
+            )
+
+        # Преобразование в словарь
+        itog_report_org1 = self.itog_report_convert_to_dict(itog_report_org1)
+        itog_report_org2 = self.itog_report_convert_to_dict(itog_report_org2)
+
+        # Добавление ИТОГО
+        itog_report_org1 = self.add_sum(itog_report_org1)
+        itog_report_org2 = self.add_sum(itog_report_org2)
+
+        # Decimal to float
+        itog_report_org1 = self.decimal_to_googlesheet(itog_report_org1)
+        itog_report_org2 = self.decimal_to_googlesheet(itog_report_org2)
+
+        report_bitrix = self.read_bitrix_base(
+            server=self.server,
+            database=self.database_bitrix,
+            user=self.user,
+            pwd=self.pwd,
+            driver=self.driver,
+            date_from=self.date_from,
+            date_to=self.date_to,
+        )
+
+
+
+        print(itog_report_org1)
+        print(itog_report_org2)
+        print(report_bitrix)
 
 
 if __name__ == '__main__':

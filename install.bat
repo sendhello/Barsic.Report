3dc4bd1957df52b90b18f1cabf0ebba4ea6baec9
Ivan Bazhenov <ivan.bazhenov@balance-pl.ru>
2021-02-02 21:57:42 +0300

    Коррекция update

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 pip install virtualenv
 virtualenv venv
-venv\Scripts\pip.exe install -r reqirements.txt
+venv\Scripts\pip.exe install -r requirements.txt
 venv\Scripts\pip.exe install --upgrade pip wheel setuptools
 venv\Scripts\pip.exe install --upgrade google-api-python-client
 cd INSTALL\KivyMD
--- update.bat
+++ update.bat
@@ -1,1 +1,2 @@
 git pull
+venv\Scripts\pip.exe install -r requirements.txt
